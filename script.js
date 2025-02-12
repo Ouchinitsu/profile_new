e85f2c2e714dbd30485cd7925a073c69c9e40528
Ouchinitsu <ouchinitsu@gmail.com>
2025-02-12 13:23:49 +0900

    to top button finished
    2/12 13:23 ou

--- script.js
+++ script.js
@@ -80,3 +80,23 @@ Array.from(sns_icons).forEach(function (sns_icon) {
     });
 });
 //#endregion
+
+//#region pop
+window.onscroll = function () { scrollFunction() };
+let scrollTopBtn = document.getElementById("top");
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollTopBtn.style.display = "block";
+    } else {
+        scrollTopBtn.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+scrollTopBtn.onclick = scrollToTop;
+//#endregion
